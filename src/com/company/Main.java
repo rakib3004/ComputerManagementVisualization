eb014144c24dfcb22b3cceb12ece0e38a97f77d7
pc <bsse1129@iit.du.ac.bd>
2020-10-22 12:03:42 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+double pie = 3.14159265358979;
+        System.out.println(pie);
+    }
+}
---
+++ src/com/company/Pie.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Pie {
+    double pieLength;
+          String pieType;
+
+    public Pie(double pieLength, String pieType) {
+        this.pieLength = pieLength;
+        this.pieType = pieType;
+    }
+
+    public double getPieLength() {
+        return pieLength;
+    }
+
+    public void setPieLength(double pieLength) {
+        this.pieLength = pieLength;
+    }
+
+    public String getPieType() {
+        return pieType;
+    }
+
+    public void setPieType(String pieType) {
+        this.pieType = pieType;
+    }
+}
