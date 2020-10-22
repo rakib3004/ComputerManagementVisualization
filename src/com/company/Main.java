0812320616721c0c216e2caeb9ac92d5e52f84ac
pc <bsse1129@iit.du.ac.bd>
2020-10-22 12:34:50 +0600

    add new value of pie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-double pie = 3.1415926535897932384626;
-        System.out.println("The value of pie : "+pie);
+  double pie = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446005118548074462379962749;
+
+        Scanner scanner = new Scanner(System.in);
+        char aAlphabet;
+  System.out.println("The value of pie : "+pie);
     }
 }
