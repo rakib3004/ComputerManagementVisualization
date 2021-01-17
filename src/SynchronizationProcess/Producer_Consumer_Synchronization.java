2211680f3328d7960306f1371743ac0c37f22c80
pc <bsse1129@iit.du.ac.bd>
2021-01-17 15:04:41 +0600

    add new Producer_Consumer_Synchronization.producer and Producer_Consumer_Synchronization.consumer and add code in there

--- src/SynchronizationProcess/Producer_Consumer_Synchronization.java
+++ src/SynchronizationProcess/Producer_Consumer_Synchronization.java
@@ -1,21 +1,60 @@
 package SynchronizationProcess;
-
+import java.util.Random;
 public class Producer_Consumer_Synchronization {
 
+static int BUFFER_SIZE = 10;
+    static int in=0,out=0;
+static int [] buffer = new int[10];
+static int  count=0;
+    static int nextProduced=0;
+    static int nextConsumed=0;
 
+    public static void printItem(){
+        int iterator;
+        for(iterator=0;iterator<BUFFER_SIZE;iterator++){
+            System.out.print(buffer[iterator]+"\t");
+        }
+    }
 
-    public void producer(){
+    public static void producer(){
+        Random producerRandom = new Random();
+while(count==BUFFER_SIZE);
 
+nextProduced = producerRandom.nextInt(70);
+buffer[in] = nextProduced;
+in = (in+1)%BUFFER_SIZE;
+count++;
+        System.out.println("\nProducer:\t");
+printItem();
+    }
+    public static void consumer(){
+        while(count==0);
+
+         nextConsumed =buffer[out];
+        out = (out+1)%BUFFER_SIZE;
+        count--;
+        System.out.println("\nConsumer:\t");
+        printItem();
 
     }
-    public void consumer(){
-
-
-
-
-    }
-
     public static void main(String[] args) {
+        Random random = new Random();
+
+
+int iterator;
+int roundNumber = 1100;
+for(iterator=0;iterator<roundNumber;iterator++){
+    if(random.nextInt(2)==0){
+      producer();
+    }
+    else{
+        consumer();
+    }
+
+}
 
     }
+
+
+
 }
