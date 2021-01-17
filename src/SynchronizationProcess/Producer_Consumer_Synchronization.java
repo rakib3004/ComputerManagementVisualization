47a01241127af5afe099e3f60868301783baf766
pc <bsse1129@iit.du.ac.bd>
2021-01-17 14:25:24 +0600

    add new Producer_Consumer_Synchronization.java for operating system problem

---
+++ src/SynchronizationProcess/Producer_Consumer_Synchronization.java
@@ -0,0 +1,21 @@
+package SynchronizationProcess;
+
+public class Producer_Consumer_Synchronization {
+
+
+
+    public void producer(){
+
+
+    }
+    public void consumer(){
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
