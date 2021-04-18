1b3e5c71a991929120be74c2a0d816cfad1374de
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-18 13:15:46 +0600

    add code in BoundedBuffer.java

---
+++ src/SynchronizationProcess/BoundedBuffer.java
@@ -0,0 +1,74 @@
+package SynchronizationProcess;
+
+ class BoundedBuffer<E>{
+    private static final int BUFFER_SIZE = 6;
+
+    private int count, in , out;
+    private E[] buffer;
+
+    public BoundedBuffer(){
+
+        count =0;
+        in =0;
+        out =0;
+        buffer = (E[]) new Object[BUFFER_SIZE];
+
+        int NUMBER_OF_THREADS = 20;
+
+
+        int i;
+
+
+        for(i=0;i<NUMBER_OF_THREADS;i++){
+
+
+        }
+    }
+
+
+
+
+
+    public synchronized void insert(E item){
+        while (count==BUFFER_SIZE){
+            try {
+                wait();
+            }
+            catch (InterruptedException interruptedException){
+
+            }
+        }
+            buffer[in] = item;
+
+
+            in = (in+1)%BUFFER_SIZE;
+            count++;
+        System.out.println("Reader reading "+count);
+
+            notify();
+
+    }
+
+    public synchronized E remove(){
+        E item;
+
+
+        while(count==0){
+            try {
+                wait();
+            }
+            catch (InterruptedException interruptedException){}
+        }
+        item = buffer[out];
+        out = (out+1) % BUFFER_SIZE;
+        count--;
+
+        System.out.println("Writer writing "+count);
+
+        return  item ;
+    }
+
+     public static void main(String[] args) {
+
+     }
+}
