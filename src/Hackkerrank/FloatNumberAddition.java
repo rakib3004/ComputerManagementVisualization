0bf8378c7a031e4e8877ae982d58ad78b721f01a
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-09 10:26:59 +0600

    add three number of diffrent three formate byte integer and float in FloatNumberAddition.java

---
+++ src/Hackkerrank/FloatNumberAddition.java
@@ -0,0 +1,19 @@
+package Hackkerrank;
+
+public class FloatNumberAddition {
+    public static void main(String[] args) {
+
+        try{
+            Float f1 = new Float("3.0");
+
+            int x = f1.intValue();
+            byte b = f1.byteValue();
+            double d = f1.doubleValue();
+            System.out.println(x+b+d);
+        }
+        catch (NumberFormatException numberFormatException){
+            System.out.println("Bad Number");
+        }
+    }
+
+}
