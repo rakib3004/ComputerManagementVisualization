e545b4e57c405b47cb9d9208e9d225b18a8dd9c4
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-09 10:41:18 +0600

    print thread in TestThread.java

---
+++ src/Hackkerrank/TestThread.java
@@ -0,0 +1,34 @@
+package Hackkerrank;
+
+class SempleDemo implements Runnable{
+
+    private Thread t;
+    private String threadName;
+
+    SempleDemo(String threadName){
+        this.threadName=threadName;
+    }
+
+    public void run(){
+        while (true)
+            System.out.println(threadName);
+    }
+    public  void start(){
+        if(t==null){
+            t = new Thread(this,threadName);
+            t.start();
+        }
+    }
+
+}
+
+
+public class TestThread {
+    public static void main(String[] args) {
+        SempleDemo A = new SempleDemo("A");
+        SempleDemo B = new SempleDemo("B");
+
+        B.start();
+        A.start();
+    }
+}
