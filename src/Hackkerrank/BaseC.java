522af57d8b077ed0885b01cf40a3c0ecb30bf98d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-09 10:17:20 +0600

    write new code for the example of interface in BaseC.java

---
+++ src/Hackkerrank/BaseC.java
@@ -0,0 +1,15 @@
+package Hackkerrank;
+
+interface BaseI {void method();}
+
+
+public class BaseC {
+    public void method(){
+        System.out.println("Inside BaseC::method");
+    }
+}
+class ImplC extends BaseC implements BaseI{
+    public static void main(String[] args) {
+        (new ImplC()).method();
+    }
+}
