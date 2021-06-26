1e877c683f53607226ec65a16aac41657ba7ebc3
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-26 23:15:38 +0600

    Devive driver ready for initiates I/O in

--- src/InterruptDrivenInputOutputCycle/CPU.java
+++ src/InterruptDrivenInputOutputCycle/CPU.java
@@ -3,7 +3,11 @@ package InterruptDrivenInputOutputCycle;
 public class CPU {
 
     public static void main(String[] args) {
+        InputOutputController inputOutputController = new InputOutputController();
 
+        System.out.println("Device Driver Start to initiate I/O");
+        System.out.println("------------------------------------");
+        inputOutputController.initiatesInputOutput();
     }
 
     void transferControlToInterruptHandler(){
