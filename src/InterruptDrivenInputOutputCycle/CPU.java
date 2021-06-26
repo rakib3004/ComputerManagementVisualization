991ef77424425f81d978796b992c138a6634b2f6
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-26 23:21:14 +0600

    initiates InputOutput, ready input, complete output and Execute checking for interrupt instruction in CPU.java

--- src/InterruptDrivenInputOutputCycle/CPU.java
+++ src/InterruptDrivenInputOutputCycle/CPU.java
@@ -5,9 +5,11 @@ public class CPU {
     public static void main(String[] args) {
         InputOutputController inputOutputController = new InputOutputController();
 
-        System.out.println("Device Driver Start to initiate I/O");
+        System.out.println("Device Driver Ready to initiate I/O");
         System.out.println("------------------------------------");
         inputOutputController.initiatesInputOutput();
+        System.out.println("CPU interrupts between instruction for checking");
+
     }
 
     void transferControlToInterruptHandler(){
--- src/InterruptDrivenInputOutputCycle/InputOutputController.java
+++ src/InterruptDrivenInputOutputCycle/InputOutputController.java
@@ -4,10 +4,17 @@ public class InputOutputController {
 
     void initiatesInputOutput(){
 
+        System.out.println("I/O Controller initiates I/O");
+        System.out.println("------------------------------------");
 
+        generateInterruptSignal();
 
     }
 void generateInterruptSignal(){
+    System.out.println("--->Input is ready!!!!!");
+    System.out.println("--->Output is complete!!!!!");
+    System.out.println("--->Interrupt Signals for Error!!!!!");
+    System.out.println("------------------------------------");
 
 }
 
