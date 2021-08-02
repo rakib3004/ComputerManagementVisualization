be7c619add60f87543e5ca2909c21c338700b1c8
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-02 20:25:05 +0600

    add  ball no, speed and bowlers name in Match

--- src/Cricket/Match.java
+++ src/Cricket/Match.java
@@ -20,5 +20,47 @@ public class Match {
         fastBowlers[1] = new FastBowler("Rubel Hossain");
         fastBowlers[2] = new FastBowler("Taskin Ahmed");
 
+        int iterator=0;
+        int jterator=0;
+
+        for(iterator=0;iterator<50;iterator++){
+
+            int bowlerType = (int) (Math.random()*3)+1;
+            int currentBowler=0;
+            if(bowlerType==1){
+                currentBowler = (int) (Math.random()*4);
+                for(jterator=1;jterator<=6;jterator++){
+                    int ballNo = (iterator*6)+jterator;
+                    System.out.println(ballNo+" th ball, " +
+                            spinBowlers[currentBowler].name+" at ," +
+                            spinBowlers[currentBowler].setBowlingSpeed(spinBowlers[currentBowler].upperSpeed,
+                            spinBowlers[currentBowler].lowerSpeed)+" Km/h");
+                }
+
+            }
+            else  if(bowlerType==2){
+                currentBowler = (int) (Math.random()*2);
+                for(jterator=1;jterator<=6;jterator++){
+                    int ballNo = (iterator*6)+jterator;
+                    System.out.println(ballNo+" th ball, " +
+                            fastMediumBowlers[currentBowler].name+" at ," +
+                            spinBowlers[currentBowler].setBowlingSpeed(spinBowlers[currentBowler].upperSpeed,
+                                    spinBowlers[currentBowler].lowerSpeed)+" Km/h");
+                }
+            }
+            else  if(bowlerType==3){
+                currentBowler = (int) (Math.random()*3);
+                for(jterator=1;jterator<=6;jterator++){
+                    int ballNo = (iterator*6)+jterator;
+                    System.out.println(ballNo+" th ball, " +
+                            spinBowlers[currentBowler].name+" at ," +
+                            spinBowlers[currentBowler].setBowlingSpeed(spinBowlers[currentBowler].upperSpeed,
+                                    spinBowlers[currentBowler].lowerSpeed)+" Km/h");
+                }
+            }
+
+
+        }
+
     }
 }
