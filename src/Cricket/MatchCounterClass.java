e14c95f313c3f8f6f3bfa562a684d05292d1434a
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-03 22:21:31 +0600

    add  new desing to get match number

--- src/Cricket/MatchCounterClass.java
+++ src/Cricket/MatchCounterClass.java
@@ -3,20 +3,31 @@ package Cricket;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class MatchCounterClass {
 
     public int getMatchNumbers(){
 
-        File matchFile = new File("/MatchCounter.txt");
-
+        File matchFile = new File("/Cricket/MatchCounter.txt");
+        char [] matchNumberChar = new char[6];
         try {
             FileReader fileReader = new FileReader(matchFile);
+            fileReader.read(matchNumberChar);
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+        int iter;
+        String matchNum="";
 
+        for(iter=0;matchNumberChar[iter]!='\0'; iter++){
+            matchNum= matchNum+matchNumberChar[iter];
+        }
+
+        int mNumber = Integer.parseInt(matchNum);
 
 
         return 1;
