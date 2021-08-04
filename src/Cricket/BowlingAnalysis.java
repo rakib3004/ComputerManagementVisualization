7a5941bc449f23fd6c7bca509a7e4117e43ff823
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-05 01:48:03 +0600

    add new  contents, information, data and analytics in BowlingAnalysis.java

--- src/Cricket/BowlingAnalysis.java
+++ src/Cricket/BowlingAnalysis.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BowlingAnalysis {
     public void BowlerStatistics(SpinBowler [] spinBowlers,
@@ -13,17 +15,35 @@ public class BowlingAnalysis {
                                          int fastBowlersNumber){
 
         MatchCounterClass matchCounterClass = new MatchCounterClass();
+        String matchDescription;
+        String iEnd;
+                iEnd="\n";
+                matchDescription="Match No:";
+        int matchNo = matchCounterClass.getMatchNumbers();
 
+        Date matchDay = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E dd.MM.yyyy");
+
+
+        // add data and information
+        matchDescription+=matchNo+", "+simpleDateFormat.format(matchDay)+"\n";
+
+        String dashString = "\n---------------------------------------------------\n";
+        String bbAnalysis = "Bowlers Bowling Analysis: \n";
+
+        // add data and information
+        matchDescription=matchDescription+dashString+bbAnalysis;
 
         int iterator;
-        System.out.println();
-        System.out.println("-------------------------------------------");
-        System.out.println();
-        System.out.println("Bowlers Bowling Analysis: ");
-
+        System.out.println(dashString);
+        System.out.println(bbAnalysis);
         for(iterator=0;iterator<spinBowlersNumber;iterator++){
           //  System.out.println("Total Speed:"+spinBowlers[iterator].totalSpeed);
             spinBowlers[iterator].averageSpeed = spinBowlers[iterator].totalSpeed/(spinBowlers[iterator].totalOver*6);
+
+            // add data and information
+            matchDescription+= "Name : "+spinBowlers[iterator].name+iEnd+"Total Overs: "+spinBowlers[iterator].totalOver+", Avg Speed: "+spinBowlers[iterator].averageSpeed+iEnd;
+
             System.out.println("Name : "+spinBowlers[iterator].name);
             System.out.println("Total Overs: "+spinBowlers[iterator].totalOver+", Avg Speed: "+spinBowlers[iterator].averageSpeed);
             System.out.println();
@@ -31,7 +51,12 @@ public class BowlingAnalysis {
 
         for(iterator=0;iterator<fastMediumBowlersNumber;iterator++){
             //System.out.println("Total Speed:"+fastMediumBowlers[iterator].totalSpeed);
+
+
             fastMediumBowlers[iterator].averageSpeed = fastMediumBowlers[iterator].totalSpeed/(fastMediumBowlers[iterator].totalOver*6);
+            // add data and information
+            matchDescription+= "Name : "+fastMediumBowlers[iterator].name+iEnd+"Total Overs: "+fastMediumBowlers[iterator].totalOver+", Avg Speed: "+fastMediumBowlers[iterator].averageSpeed+iEnd;
+
             System.out.println("Name : "+fastMediumBowlers[iterator].name);
             System.out.println("Total Overs: "+fastMediumBowlers[iterator].totalOver+", Avg Speed: "+fastMediumBowlers[iterator].averageSpeed);
             System.out.println();
@@ -39,16 +64,21 @@ public class BowlingAnalysis {
 
         for(iterator=0;iterator<fastBowlersNumber;iterator++){
            // System.out.println("Total Speed:"+fastBowlers[iterator].totalSpeed);
+
             fastBowlers[iterator].averageSpeed = fastBowlers[iterator].totalSpeed/(fastBowlers[iterator].totalOver*6);
+            // add data and information
+            matchDescription+= "Name : "+fastBowlers[iterator].name+iEnd+"Total Overs: "+fastBowlers[iterator].totalOver+", Avg Speed: "+fastBowlers[iterator].averageSpeed+iEnd;
+
+
             System.out.println("Name : "+fastBowlers[iterator].name);
             System.out.println("Total Overs: "+fastBowlers[iterator].totalOver+", Avg Speed: "+fastBowlers[iterator].averageSpeed);
             System.out.println();
         }
+  matchDescription+=dashString+iEnd+iEnd;
 
-        String matchDescription="testing------------------> ";
-        int matchNo = matchCounterClass.getMatchNumbers();
-        matchDescription+=matchNo+"\n";
       File recordFile  = new File("MatchRecordBook.txt");
+
+
         /*  FileWriter fileWriter = null;
         try {
             fileWriter = new FileWriter(recordFile);
