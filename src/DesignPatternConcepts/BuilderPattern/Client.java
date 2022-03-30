1c07a7346bdea8b1bf1a433e9e5306b248ea4ba3
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-30 20:11:44 +0600

    add user, client and address

--- src/DesignPatternConcepts/BuilderPattern/Client.java
+++ src/DesignPatternConcepts/BuilderPattern/Client.java
@@ -1,4 +1,38 @@
 package DesignPatternConcepts.BuilderPattern;
 
+import java.time.LocalDate;
+
+//This is our client which also works as "director"
 public class Client {
+
+    public static void main(String[] args) {
+        User user = createUser();
+        UserDTOBuilder builder = new UserWebDTOBuilder();
+        //Client has to provide director with concrete builder
+        UserDTO dto = directBuild(builder, user);
+        System.out.println(dto);
+    }
+
+
+    private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+        return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName())
+                .withAddress(user.getAddress())
+                .withBirthday(user.getBirthday())
+                .build();
+    }
+
+    public static User createUser() {
+        User user = new User();
+        user.setBirthday(LocalDate.of(1979, 07, 6));
+        user.setFirstName("Pedram");
+        user.setLastName("Karevan");
+        Address address = new Address();
+        address.setHouseNumber("100");
+        address.setStreet("State Street");
+        address.setCity("Tehran");
+        address.setState("Iran");
+        address.setZipcode("2375");
+        user.setAddress(address);
+        return user;
+    }
 }
