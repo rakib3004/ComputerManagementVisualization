734af246beb3e2e7d8b3d7d73d16deac472860c2
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-31 15:37:14 +0600

    add new post

--- src/DesignPatternConcepts/ObserverPattern/Students.java
+++ src/DesignPatternConcepts/ObserverPattern/Students.java
@@ -9,6 +9,6 @@ public class Students implements GoogleClassRoomStudents {
     }
 
     public void update(String classroomName, String postDetails) {
-        System.out.println(subscriberName + ", there is a post for you from" + classroomName + "Here is the details" + postDetails);
+        System.out.println(subscriberName + ", there is a post for you from" + classroomName + "Here is the details about post" + postDetails);
     }
 }
