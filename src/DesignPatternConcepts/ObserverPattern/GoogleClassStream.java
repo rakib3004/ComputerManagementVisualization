7cc3b36b9f42ea883de5183c22c8e226e4cb8a00
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-04 11:30:27 +0600

    add post uploaded

--- src/DesignPatternConcepts/ObserverPattern/GoogleClassStream.java
+++ src/DesignPatternConcepts/ObserverPattern/GoogleClassStream.java
@@ -8,15 +8,35 @@ public class GoogleClassStream {
         InitDataBase initDataBase = new InitDataBase();
 
         ArrayList<EducationalGoogleClassRoom> educationalGoogleClassRooms = initDataBase.initEducationalClassRoom();
+        ArrayList<GoogleClassRoomStudents> googleClassRoomStudents = initDataBase.initGoogleClassRoomStudents();
 
-        System.out.println("\nDay 1 Announcements: ");
-        designPattern.postUploaded("Code Smells");
-        webTechnology.postUploaded("Angular Framework");
+        int firstClassIndex,lastClassIndex,firstStudentIndex,lastStudentIndex;
+        firstClassIndex=0;
+        lastClassIndex=5;
+        firstStudentIndex=0;
+        lastStudentIndex=29;
+        int steamLength=120;
+       for(int i=0; i<steamLength;i++){
 
 
-        System.out.println("\nDay 2 Announcements: ");
-        designPattern.postUploaded("Structural Design Pattern");
-        webTechnology.postUploaded(".NET Framework");
+
+           int theClassIndex=(int) ((Math.random() * (lastClassIndex - firstClassIndex)) + firstClassIndex);
+           int theStudentIndex=(int)((Math.random() * (lastStudentIndex - firstStudentIndex)) + firstStudentIndex);
+
+           educationalGoogleClassRooms.get(theClassIndex).subscribe(googleClassRoomStudents.get(theStudentIndex));
+
+       }
+        ArrayList<String> dayOne = new ArrayList<>();
+       dayOne.add("Builder Pattern");
+       dayOne.add(".NET Framework");
+       dayOne.add("File System");
+       dayOne.add("IT workers Ethics");
+       dayOne.add("Init Project");
+       dayOne.add("Communications Culture");
+
+       for(int i=0;i<=lastClassIndex;i++){
+           educationalGoogleClassRooms.get(i).postUploaded(dayOne.get(i));
+       }
 
 
     }
