ae965bfd973e733f94e0d503b45c2df51a404036
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-04 14:54:37 +0600

    done!

--- src/DesignPatternConcepts/BuilderPattern/AdministrationBuilding.java
+++ src/DesignPatternConcepts/BuilderPattern/AdministrationBuilding.java
@@ -4,11 +4,9 @@ public class AdministrationBuilding {
     public static void main(String[] args) {
 
         Department softwareEngineering
-                = new DepartmentBuilder().setDepartmentName("Information Technology").setDepartmentBuildingName("Computer Centre").setDepartmentAddress("Mokaram Educational Area, University of Dhaka, Dhaka -1000").setTotalClassNumber("5").setDepartmentDegreeName("BIT").getDepartmentDetails();
+                = new DepartmentBuilder().setDepartmentName("Information Technology").setDepartmentBuildingName("Computer Centre").setDepartmentAddress("IIT Building, Mokaram Area, University of Dhaka, Dhaka -1000").setTotalClassNumber("9").setDepartmentDegreeName("BSSE").getDepartmentDetails();
         System.out.println(softwareEngineering.toString());
 
-
-        System.out.println(softwareEngineering.toString());
 
     }
 }
