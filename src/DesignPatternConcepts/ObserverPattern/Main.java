da909f836dd5a3d6788209aa3d55be80412b44c1
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-04 08:05:36 +0600

    add new code

--- src/DesignPatternConcepts/ObserverPattern/Main.java
+++ src/DesignPatternConcepts/ObserverPattern/Main.java
@@ -6,10 +6,10 @@ public class Main {
         EducationalGoogleClassRoom designPattern = new EducationalGoogleClassRoom("Design Pattern");
         EducationalGoogleClassRoom webTechnology = new EducationalGoogleClassRoom("Web Technology");
 
-        Students Muktar = new Students(" Muktar ");
-        Students Rakib = new Students(" Rakib ");
-        Students Rahat = new Students(" Rahat ");
-        Students Khairul = new Students(" Khairul ");
+        Students Muktar = new Students(" Muktar");
+        Students Rakib = new Students(" Rakib");
+        Students Rahat = new Students(" Rahat");
+        Students Khairul = new Students(" Khairul");
 
         designPattern.subscribe(Muktar);
         designPattern.subscribe(Rakib);
@@ -22,7 +22,7 @@ public class Main {
         designPattern.unsubscribe(Rakib);
         webTechnology.unsubscribe(Khairul);
 
-        System.out.println("\nDay 2 Announcements: ");
+        System.out.println("\nDay 2 Annoucements: ");
         designPattern.postUploaded("Structural Design Pattern");
         webTechnology.postUploaded(".NET Framework");
 
