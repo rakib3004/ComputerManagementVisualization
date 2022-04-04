26f9686fce38f734b2cf3f6bf02f6178d73a9603
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-04 10:43:54 +0600

    start to add bsse students and all classes

--- src/DesignPatternConcepts/ObserverPattern/GoogleClassStream.java
+++ src/DesignPatternConcepts/ObserverPattern/GoogleClassStream.java
@@ -5,16 +5,57 @@ public class GoogleClassStream {
 
         EducationalGoogleClassRoom designPattern = new EducationalGoogleClassRoom("Design Pattern");
         EducationalGoogleClassRoom webTechnology = new EducationalGoogleClassRoom("Web Technology");
+        EducationalGoogleClassRoom databaseManagementSystemII = new EducationalGoogleClassRoom("Database Management System II");
+        EducationalGoogleClassRoom professionalEthics = new EducationalGoogleClassRoom("Professional Ethics");
+        EducationalGoogleClassRoom softwareProjectLabII = new EducationalGoogleClassRoom("Software Project Lab II");
+        EducationalGoogleClassRoom businessCommunications = new EducationalGoogleClassRoom("Business Communications");
+
+
+
+        GoogleClassRoomStudents bsse1111 = new GoogleClassRoomStudents("BSSE 1101");
+        GoogleClassRoomStudents bsse1105 = new GoogleClassRoomStudents("BSSE 1102");
+        GoogleClassRoomStudents bsse1128 = new GoogleClassRoomStudents("BSSE 1103");
+        GoogleClassRoomStudents bsse1125 = new GoogleClassRoomStudents("BSSE 1104");
+        GoogleClassRoomStudents bsse1115 = new GoogleClassRoomStudents("BSSE 1105");
+
+
+        GoogleClassRoomStudents bsse1129 = new GoogleClassRoomStudents("BSSE 1106");
+        GoogleClassRoomStudents bsse1101 = new GoogleClassRoomStudents("BSSE 1107");
+        GoogleClassRoomStudents bsse1114 = new GoogleClassRoomStudents("BSSE 1108");
+        GoogleClassRoomStudents bsse1110 = new GoogleClassRoomStudents("BSSE 1109");
+        GoogleClassRoomStudents bsse1109 = new GoogleClassRoomStudents("BSSE 1110");
+
+        GoogleClassRoomStudents bsse1111 = new GoogleClassRoomStudents("BSSE 1111");
+        GoogleClassRoomStudents bsse1105 = new GoogleClassRoomStudents("BSSE 1112");
+        GoogleClassRoomStudents bsse1128 = new GoogleClassRoomStudents("BSSE 1113");
+        GoogleClassRoomStudents bsse1125 = new GoogleClassRoomStudents("BSSE 1114");
+        GoogleClassRoomStudents bsse1115 = new GoogleClassRoomStudents("BSSE 1115");
 
 
         GoogleClassRoomStudents bsse1129 = new GoogleClassRoomStudents("BSSE 1129");
         GoogleClassRoomStudents bsse1101 = new GoogleClassRoomStudents("BSSE 1101");
-        GoogleClassRoomStudents bsse1116 = new GoogleClassRoomStudents("BSSE 1116");
+        GoogleClassRoomStudents bsse1114 = new GoogleClassRoomStudents("BSSE 1114");
         GoogleClassRoomStudents bsse1110 = new GoogleClassRoomStudents("BSSE 1110");
         GoogleClassRoomStudents bsse1109 = new GoogleClassRoomStudents("BSSE 1109");
 
+        GoogleClassRoomStudents bsse1116 = new GoogleClassRoomStudents("BSSE 1116");
+        GoogleClassRoomStudents bsse1105 = new GoogleClassRoomStudents("BSSE 1105");
+        GoogleClassRoomStudents bsse1128 = new GoogleClassRoomStudents("BSSE 1128");
+        GoogleClassRoomStudents bsse1125 = new GoogleClassRoomStudents("BSSE 1125");
+        GoogleClassRoomStudents bsse1115 = new GoogleClassRoomStudents("BSSE 1115");
+
+
+        GoogleClassRoomStudents bsse1129 = new GoogleClassRoomStudents("BSSE 1129");
+        GoogleClassRoomStudents bsse1101 = new GoogleClassRoomStudents("BSSE 1101");
+        GoogleClassRoomStudents bsse1114 = new GoogleClassRoomStudents("BSSE 1114");
+        GoogleClassRoomStudents bsse1110 = new GoogleClassRoomStudents("BSSE 1110");
+        GoogleClassRoomStudents bsse1109 = new GoogleClassRoomStudents("BSSE 1109");
+
+
+
+
         designPattern.subscribe(bsse1129);
-        designPattern.subscribe(bsse1116);
+        designPattern.subscribe(bsse1114);
         webTechnology.subscribe(bsse1110);
         webTechnology.subscribe(bsse1109);
         designPattern.subscribe(bsse1101);
@@ -23,7 +64,7 @@ public class GoogleClassStream {
         System.out.println("\nDay 1 Announcements: ");
         designPattern.postUploaded("Code Smells");
         webTechnology.postUploaded("Angular Framework");
-        designPattern.unsubscribe(bsse1116);
+        designPattern.unsubscribe(bsse1114);
         webTechnology.unsubscribe(bsse1109);
 
         System.out.println("\nDay 2 Announcements: ");
